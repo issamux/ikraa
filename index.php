8734400f9b8929ca8a59856acd01d144e2fa2c9b
issamux <issamux@gmail.com>
2013-12-04 01:20:33 +0000

    initial commit
    add sample index file and bootstrap files

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title> E learning Plateform<title>
+    
+    <!-- Bootstrap core CSS -->
+    <link href="bootsrap/css/bootstrap.css" rel="stylesheet">
+
+    
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  
+ 
+  <div class="container">
+
+ 	<form id="myForm" action="#" method="post">
+ 	username: <input type="text" name="user"><br>
+ 	password: <input type="password" name="pass"><br>
+ 
+   
+  <br/>
+ <input type="submit" value="login">
+    </form>
+    
+      </div> 
+      <!-- /container -->
+      <!-- Bootstrap core JavaScript
+    ================================================== 
+    -->
+    <script  src="http//code.jquery.com/jquery-1.10.2.min.js" /> 
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.2/angular.min.js"></script>
+
+    
+  </body>
+</html>
+
